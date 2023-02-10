6bbd6f504949d9abb26dbbfc61b98a8271638eb6
Daniel Winther <danielwintherjensen@gmail.com>
2023-02-10 16:00:33 +0100

    Cleanup code

--- src/cronjobs/handleSkatsPositivliste.js
+++ src/cronjobs/handleSkatsPositivliste.js
@@ -1,44 +1,39 @@
 const https = require("https");
 const fs = require("fs");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const { JSDOM } = require("jsdom");
 
-module.exports = function () {
-  (async () => {
-    // Scrapes the href and date of the list from Skats website
-    const response = await fetch(
-      process.env.SKAT_URL + "data.aspx?oid=2244641"
-    );
-    const body = await response.text();
-    const dom = new JSDOM(body);
-    const element = dom.window.document.querySelector(
-      "#toggleHdr4u-3 a:first-child"
-    );
+module.exports = async function () {
+  // Scrapes the href and date of the list from Skats website
+  const response = await fetch(process.env.SKAT_URL + "data.aspx?oid=2244641");
+  const body = await response.text();
+  const dom = new JSDOM(body);
+  const element = dom.window.document.querySelector(
+    "#toggleHdr4u-3 a:first-child"
+  );
 
-    // Saves the file on the server and sets the modified date accordingly
-    https.get(process.env.SKAT_URL + element.href, (response) => {
-      const fileStream = fs.createWriteStream(
-        "./public/xlxs/skats-positivliste.xlxs"
+  // Saves the file on the server and sets the modified date accordingly
+  https.get(process.env.SKAT_URL + element.href, (response) => {
+    const fileStream = fs.createWriteStream(
+      "./public/xlxs/skats-positivliste.xlxs"
+    );
+    response.pipe(fileStream);
+    fileStream.on("finish", () => {
+      const formatDate = element.title
+        .split("listen ")[1]
+        .split(".xlsx")[0]
+        .match(/^(\d{2})(\d{2})(\d{4})/);
+      const lastModified = new Date(
+        formatDate[3],
+        formatDate[2] - 1,
+        formatDate[1]
       );
-      response.pipe(fileStream);
-      fileStream.on("finish", () => {
-        const formatDate = element.title
-          .split("listen ")[1]
-          .split(".xlsx")[0]
-          .match(/^(\d{2})(\d{2})(\d{4})/);
-        const lastModified = new Date(
-          formatDate[3],
-          formatDate[2] - 1,
-          formatDate[1]
-        );
-        fs.utimesSync(
-          "./public/xlxs/skats-positivliste.xlxs",
-          lastModified,
-          lastModified
-        );
-        fileStream.close();
-        console.info("Download finished");
-      });
+      fs.utimesSync(
+        "./public/xlxs/skats-positivliste.xlxs",
+        lastModified,
+        lastModified
+      );
+      fileStream.close();
+      console.info("Download finished");
     });
-  })();
+  });
 };
