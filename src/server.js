6bd2cfcfce3fee6614f201cc258e91ec0a4ef137
Daniel Winther <danielwintherjensen@gmail.com>
2023-02-17 23:05:04 +0100

    Small refactor

--- src/server.js
+++ src/server.js
@@ -3,18 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const cron = require("node-cron");
 const skatsPositivlisteRoute = require("./routes/skatsPositivlisteRoute.js");
-const handleSkatsPositivliste = require("./cronjobs/handleSkatsPositivliste.js");
+const handleSkatsPositivliste = require("./jobs/handleSkatsPositivliste.js");
 
 // Constants
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
-// App
-const app = express();
-
+// Cron jobs
 cron.schedule("0 0 * * * ", function () {
   handleSkatsPositivliste();
 });
+
+// App
+const app = express();
 
 app.use(express.static("public"));
 app.use(function (req, res, next) {
@@ -22,6 +23,7 @@ app.use(function (req, res, next) {
 });
 app.set("view engine", "pug");
 
+// Routes
 app.use("/", skatsPositivlisteRoute);
 
 app.listen(PORT, HOST);
