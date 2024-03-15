b28edcfbf9c4a198779ecd5b4c05b3d4f51597bc
Daniel Winther <danielwintherjensen@gmail.com>
2024-03-15 22:59:20 +0100

    Adsense consent

--- src/public/js/gtag.js
+++ src/public/js/gtag.js
@@ -3,10 +3,10 @@ function gtag() {
   dataLayer.push(arguments);
 }
 gtag("consent", "default", {
-  ad_user_data: "denied",
-  ad_personalization: "denied",
-  ad_storage: "denied",
-  analytics_storage: "denied",
+  ad_user_data: "granted",
+  ad_personalization: "granted",
+  ad_storage: "granted",
+  analytics_storage: "granted",
   wait_for_update: 500,
 });
 gtag("js", new Date());
