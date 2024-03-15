dbdd48d1dbf54516b347d732c1469b3efffeb7b3
Daniel Winther <danielwintherjensen@gmail.com>
2024-03-15 19:34:45 +0100

    cookiebot banner

--- src/public/js/gtag.js
+++ src/public/js/gtag.js
@@ -2,13 +2,6 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
-gtag("consent", "default", {
-  ad_user_data: "granted",
-  ad_personalization: "granted",
-  ad_storage: "granted",
-  analytics_storage: "granted",
-  wait_for_update: 500,
-});
 gtag("js", new Date());
 
 gtag("config", "G-BL9LRG9CK6");
