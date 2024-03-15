8d42e26048d454afc84854cd36c10171bd682c8f
Daniel Winther <danielwintherjensen@gmail.com>
2024-03-15 19:22:40 +0100

    Test gtag consent

--- src/public/js/gtag.js
+++ src/public/js/gtag.js
@@ -17,23 +17,26 @@ document.addEventListener("DOMContentLoaded", initialize);
 
 function initialize() {
   const grantButton = document.querySelector(".fc-cta-consent");
-  grantButton.addEventListener("click", function (event) {
-    console.log("ok");
-    localStorage.setItem("consentGranted", "true");
+  if (grantButton) {
+    grantButton.addEventListener("click", function (event) {
+      console.log("ok");
+      localStorage.setItem("consentGranted", "true");
 
-    gtag("consent", "update", {
-      ad_user_data: "granted",
-      ad_personalization: "granted",
-      ad_storage: "granted",
-      analytics_storage: "granted",
+      gtag("consent", "update", {
+        ad_user_data: "granted",
+        ad_personalization: "granted",
+        ad_storage: "granted",
+        analytics_storage: "granted",
+      });
+
+      // Load gtag.js script.
+      const gtagScript = document.createElement("script");
+      gtagScript.async = true;
+      gtagScript.src =
+        "https://www.googletagmanager.com/gtag/js?id=G-BL9LRG9CK6";
+
+      const firstScript = document.getElementsByTagName("script")[0];
+      firstScript.parentNode.insertBefore(gtagScript, firstScript);
     });
-
-    // Load gtag.js script.
-    const gtagScript = document.createElement("script");
-    gtagScript.async = true;
-    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-BL9LRG9CK6";
-
-    const firstScript = document.getElementsByTagName("script")[0];
-    firstScript.parentNode.insertBefore(gtagScript, firstScript);
-  });
+  }
 }
